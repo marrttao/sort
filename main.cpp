76489faaba321dc815e867abc75060b68580b1bc
Maris Shardyko <marrttaoburberry@gmail.com>
2024-12-31 17:32:37 +0200

    completed task 4

--- main.cpp
+++ main.cpp
@@ -1,6 +1,66 @@
 #include <iostream>
+#include <random>
 using namespace std;
 
+int split_in_two_parts(int arr[], int low, int high) {
+    int pivot = arr[high];
+    int i = low - 1;
+
+    for (int j = low; j < high; j++) {
+        if (arr[j] <= pivot) {
+            i++;
+            swap(arr[i], arr[j]);
+        }
+    }
+    swap(arr[i + 1], arr[high]);
+    return i + 1;
+}
+
+void quick_sort(int arr[], int low, int high) {
+    if (low < high) {
+        int pi = split_in_two_parts(arr, low, high);
+
+        quick_sort(arr, low, pi - 1);
+        quick_sort(arr, pi + 1, high);
+    }
+}
+
+void random_array(int* arr, int size) {
+    random_device rd;
+    mt19937 mt(rd());
+    uniform_int_distribution<int> dist(1, 100);
+    for (int i = 0; i < size; i++) {
+        arr[i] = dist(mt);
+    }
+}
+
 int main() {
-	return 0;
+    int size;
+    cout << "Size of array: ";
+    cin >> size;
+
+    if (size <= 0) {
+        cout << "Invalid array size!" << endl;
+        return 1;
+    }
+
+    int* arr = new int[size];
+    random_array(arr, size);
+
+    cout << "Before" << endl;
+    for (int i = 0; i < size; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    quick_sort(arr, 0, size - 1);
+
+    cout << "After" << endl;
+    for (int i = 0; i < size; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    delete[] arr;
+    return 0;
 }
